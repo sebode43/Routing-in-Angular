42280a8d3a5ad3f8a7f0febf486a1b58814618fa
max-student <max-student@maxtrain.com>
2020-03-20 15:54:28 -0400

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './misc/home/home.component';
+import { AboutComponent } from './misc/about/about.component';
+import { HelpComponent } from './misc/help/help.component';
+import { ContactComponent } from './misc/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/help", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "help", component: HelpComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "**", component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
